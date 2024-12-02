1761aef0399a08af9dc013f8eabbc520f812da0c
Boy1Lane <85323910+Boy1Lane@users.noreply.github.com>
2024-12-02 20:44:37 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,29 @@
+﻿import socket
+
+HOST = '192.168.150.5'  # Địa chỉ server
+PORT = 65431        # Cổng server đang lắng nghe
+
+# Tạo socket TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (HOST, PORT)
+print('Connecting to {} port {}'.format(*server_address))
+s.connect(server_address)
+
+try:
+    while True:
+        # Gửi dữ liệu từ client
+        msg = input('Client: ')
+        s.sendall(msg.encode("utf8"))
+
+        if msg.lower() == "quit":  # Client thoát nếu gửi "quit"
+            print("Closing connection.")
+            break
+
+        # Nhận phản hồi từ server
+        data = s.recv(1024)
+        if not data:
+            print("Server disconnected.")
+            break
+        print('Server:', data.decode("utf8"))
+finally:
+    s.close()
